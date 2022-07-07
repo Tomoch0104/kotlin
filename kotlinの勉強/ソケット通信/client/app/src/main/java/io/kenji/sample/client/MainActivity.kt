98dc8b24aacfe4cd052b8d2812608a94617521f1
Tomoch0104 <to0104mo2517@icloud.com>
2022-07-07 18:06:47 +0900

    first maker file

---
+++ kotlinの勉強/ソケット通信/client/app/src/main/java/io/kenji/sample/client/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.kenji.sample.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
